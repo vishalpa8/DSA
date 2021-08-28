a0aa3640308f6524483f0aa63e3efb3e63278320
Vishal pandey <60701275+vishalpa8@users.noreply.github.com>
2021-08-28 10:26:09 +0530

    Optimized Sorting

--- Sorting/BestSorting.cpp
+++ Sorting/BestSorting.cpp
@@ -1,68 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long 
-#define fi first
-#define se second
-#define endl '\n'
-#define fast_io ios_base::sync_with_stdio(0); cin.tie(0)
 
-void bubbleSort(vector<int> v){
-	bool isSorted = false;
-	int counter = 0;
-	while(!isSorted){
-		for(int i=0; i<v.size()-1-counter; i++){
-			isSorted = true;
-			if(v[i] > v[i+1]){
-				isSorted = false;
-				swap(v[i],v[i+1]);
-			}
-		}
-		counter++;
-	}
-
-	for(auto i :  v){
-		cout << i << " ";
-	}
-	cout << endl;
-}
-
-void insertionSort(vector<int> v){
-	for(int i=1; i<v.size(); i++){
-		int j=i;
-		while( j > 0 && v[j] < v[j-1]){
-			swap(v[j],v[j-1]);
-			j--;
-		}	
-	}
-
-	for(auto i : v){
-		cout << i << " ";
-	}
-	cout << endl;
-}
-
-void selectionSort(vector<int> v){
-	int currentIdx = 0;
-
-	while(currentIdx < v.size()){
-		int smallestIdx = currentIdx;
-		for(int i = currentIdx+1; i < v.size(); i++){
-			if(v[smallestIdx] > v[i]){
-				smallestIdx = i;
-			}
-		}
-		swap(v[smallestIdx],v[currentIdx]);
-		currentIdx++;
-	}
-
-	for(auto i : v){
-		cout << i << " ";
-	}
-
-	cout << endl;
-}
-
-int main(){
+void selectionsort(){
 	int n;
 	cin >> n;
 
@@ -72,8 +11,78 @@ int main(){
 		cin >> v[i];
 	}
 
-	bubbleSort(v);
-	insertionSort(v);
-	selectionSort(v);
+	int currentIdx = 0;
+
+	while(currentIdx < n){
+		int smallestIdx = currentIdx;
+
+		for(int i=currentIdx+1; i<n; i++){
+			if(v[smallestIdx] > v[i]){
+				smallestIdx = i;
+			}
+		}
+		swap(v[currentIdx],v[smallestIdx]);
+		currentIdx++;
+	}
+
+	for(auto i :  v){
+		cout << i << " ";
+	}
+}
+
+void insertionsort(){
+	int n;
+	cin >> n;
+	vector<int> v(n);
+
+	for(int i=0; i<n; i++){
+		cin >> v[i];
+	}
+
+	for(int i=1; i<n; i++){
+		int j = i-1;
+		int key = v[i];
+		while(j >= 0 && v[j] > key){
+			v[j+1] = v[j];
+			j--;
+		}
+		v[j+1] = key;
+	}
+
+	for(auto i : v){
+		cout << i << " ";
+	}
+}
+
+void bubblesort(){
+	
+	int n;
+	cin >> n;
+	vector<int> v(n);
+
+	for(int i=0; i<n; i++){
+		cin >> v[i];
+	}
+
+	for(int i=1; i<n; i++){
+		bool is_sorted = true;
+		for(int j=0; j<n; j++){
+			if(v[j] > v[j+1]){
+				swap(v[j],v[j+1]);
+				is_sorted = false;
+			}
+		}
+		if(is_sorted) break;
+	}
+
+	for(auto i : v){
+		cout << i << " ";
+	}
+}
+
+int main(){
+	insertionsort();
+	selectionsort();
+	bubblesort();
 	return 0;
 }
